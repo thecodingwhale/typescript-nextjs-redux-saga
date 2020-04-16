6645ef72fa308f91c0da88969ce6602c777a5a98
Aldren Terante <aldren.terante@gmail.com>
2020-04-16 15:07:48 +0800

    feat: add simple component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import Hello from '../src/components/Hello/Hello';
 
-const IndexRoot: React.FC = () => {
-  return <div>Hello</div>;
+const IndexRoot: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
 };
 
 export default IndexRoot;
---
+++ src/components/Hello/Hello.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface HelloProps {
+  name?: string;
+}
+
+const Hello: React.FC<HelloProps> = ({ name }: HelloProps): JSX.Element => {
+  return <h1>Hello, {name}</h1>;
+};
+
+Hello.defaultProps = {
+  name: 'Stranger'
+};
+
+export default Hello;
