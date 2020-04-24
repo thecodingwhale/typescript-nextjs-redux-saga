33ceb38e67f62dcd3a0ce513ca28941c6c71f056
Aldren Terante <aldren.terante@gmail.com>
2020-04-24 11:27:06 +0800

    feat: add module resolver for utils

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import Hello from '@components/Hello/Hello'
 import { onFormLoginLogout } from '@containers/FormLogin/action'
 import { useSelector, useDispatch } from 'react-redux'
-import { auth } from '../src/utils/auth'
+import { auth } from '@utils/auth'
 
 const Profile = () => {
   const dispatch = useDispatch()
